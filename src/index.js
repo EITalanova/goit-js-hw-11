0e36c23423832d42859684779f5f462e7597bf1f
EITalanova <elenabelichenko77@gmail.com>
2023-02-05 16:58:20 -0800

    Added pagination, btn LoadMore, library SimpleLightbox

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 import NewApiService from './service-gallery';
+import Notiflix from 'notiflix';
+// import loadMoreBtn from './loadMoreBtn';
+import LoadMoreBtn from './loadMoreBtn';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+var lightbox = new SimpleLightbox('.gallery-link');
 
 const refs = {
   searchInput: document.querySelector('input[name="searchQuery"]'),
   searchForm: document.querySelector('.search-form'),
+  // loadMoreBtn: document.querySelector('.load-more'),
   galleryBox: document.querySelector('.gallery'),
 };
 
 const newApiService = new NewApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '.load-more',
+  hidden: true,
+});
 
 refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onPageWithGalerry);
 
 function onSearch(evt) {
   evt.preventDefault();
 
-  // clearContainer();
-  // newApiService.namePic = evt.currentTarget.elements.namePic.value;
-//   console.log(newApiService.namePic);
+  const a = refs.searchInput.value;
 
-    newApiService.fetchGallery().then(hits => refs.galleryBox.insertAdjacentHTML('beforeend', markupGallery(hits))
-    );
+  clearContainer();
+  // newApiService.namePic = evt.currentTarget.elements.namePic.value;
+  newApiService.resetPage();
+  onPageWithGalerry();
+}
+
+function onPageWithGalerry() {
+  loadMoreBtn.hide();
+
+  newApiService.fetchGallery().then(hits => {
+    refs.galleryBox.insertAdjacentHTML('beforeend', markupGallery(hits));
+    lightbox.refresh();
+    loadMoreBtn.show();
+  });
 }
 
 function clearContainer() {
@@ -30,7 +53,7 @@ function markupGallery(hits) {
     .map(
       hit =>
         `<div class="photo-card">
-  <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+  <a href="${hit.largeImageURL}" class="gallery-link"><img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">${hit.likes}
       <b>Likes</b>
@@ -49,5 +72,3 @@ function markupGallery(hits) {
     )
     .join('');
 }
-
-
--- src/service-gallery.js
+++ src/service-gallery.js
@@ -1,16 +1,42 @@
+import Notiflix from 'notiflix';
 export default class NewApiService {
   constructor() {
+    this.BASE_URL = 'https://pixabay.com/api/';
+    this.KEY = '33345344-6416458ee2baf9633eccc50a4';
     this.searchNamePic = '';
+    this.imageType = 'photo';
+    this.orientation = 'horizontal';
+    this.safesearch = true;
+    this.page = 1;
+    this.perPage = 100;
   }
 
   fetchGallery() {
-    const url = `https://pixabay.com/api/?key=33345344-6416458ee2baf9633eccc50a4&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
-    console.log('1');
+    const a = document.querySelector('input[name="searchQuery"]').value;
+    const url = `${this.BASE_URL}?key=${this.KEY}&q=${a}&image_type=${this.imageType}&orientation=${this.orientation}&safesearch=${this.safesearch}&page=${this.page}&per_page=${this.perPage}`;
+
     return fetch(url)
       .then(response => response.json())
       .then(data => {
+        if (data.totalHits === 0) {
+          Notiflix.Notify.warning(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else if (Math.floor(data.totalHits / this.perPage) === this.page) {
+          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        }
+        this.incrementPage();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
         return data.hits;
       });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
   }
 
   get namePic() {
